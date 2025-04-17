d340e04c246d0281c1d53796f9b8223085d526f7
Isa-cat <im.marquardts@gmail.com>
2025-04-17 13:14:07 +0200

    Create PatternIndices.py

---
+++ PatternIndices.py
@@ -0,0 +1,21 @@
+#pattern_counter: count how often a specific pattern appears in a sequence
+genome_file=open('Vibrio_cholerae.txt', 'r')
+genome=genome_file.readlines()
+
+
+def PatternIndices(pattern, sequence):
+    """
+    in theory, the counter would have to 'wrap' to also go from last character in string to the first ones to check
+    that the pattern can not be completed that way?
+        
+    """
+    length=len(pattern)
+    indices=str()
+    for i in range (len(sequence)-length+1): 
+        if sequence[i:i+length] == pattern:
+            indices = indices+str(i)+' '
+        else:
+            pass
+    return indices
+
+PatternIndices('CTTGATCAT', genome)
