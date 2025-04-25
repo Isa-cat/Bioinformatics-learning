dc909a5b2ee458dd1bffd47ae65134c125b7e870
Isa-cat <im.marquardts@gmail.com>
2025-04-25 15:06:42 +0200

    Update ApproximatePatternIndices.py

--- ApproximatePatternIndices.py
+++ ApproximatePatternIndices.py
@@ -3,8 +3,7 @@ def ApproximatePatternCount(pattern, text, dev):
     length=len(pattern)
     indices=str()
     for i in range (len(text)-length+1): 
-        hamming_dist=Hamming_Distance(text[i:i+length], pattern)
-        if (hamming_dist<=dev) or (text[i:i+length] == pattern): 
+        if (Hamming_Distance(text[i:i+length], pattern)<=dev): 
             indices = indices+str(i)+' '
         else:
             pass
@@ -24,5 +23,5 @@ def Hamming_Distance(seq1,seq2):
 genome_file=open('dataset_30278_4.txt','r')
 filedata=genome_file.readlines()
 genome_file.close()
-ApproximatePatternCount(filedata[0], filedata[1],int(filedata[2]))
+ApproximatePatternCount(filedata[0].strip(), filedata[1].strip(),int(filedata[2].strip())) #strip() to remove any whitespace characters
 #ApproximatePatternCount('ATTCTGGA', 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT',3)
