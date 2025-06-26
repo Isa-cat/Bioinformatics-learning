e6b1aaa8ce910e8e6217b4d3b293bad4a8ba1523
Isa-cat <im.marquardts@gmail.com>
2025-06-26 13:25:59 +0200

    Update sequence_kmer_composition.py
    Add string reconstruction from kmers (easy version)

--- sequence_kmer_composition.py
+++ sequence_kmer_composition.py
@@ -11,3 +11,20 @@ def text_comp(k,text):
 
 f = open("dataset_30153_3 (1).txt", 'r').read().strip().splitlines('\n ')
 text_comp(int(f[0]),f[1])
+
+
+"""
+String reconstruction problem, easy version: kmers are given as input in the order so that the last k-1 bases of kmer(i) equal the k-1 first bases of kmer(i+1)
+input: collection of kmers
+output: reconstructed string
+"""
+
+def kmers_to_string(kmers):
+    string=kmers[0]
+    for pattern in kmers[1:]:
+        string+=pattern[-1]
+    return string
+
+print(kmers_to_string(f[0].split(' '))) 
+#print(f[0].split(' '))
+f = open("dataset_30182_3.txt", 'r').read().strip().splitlines('\n ')
