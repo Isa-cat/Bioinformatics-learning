b301d410deaea9592e38ea516b621fee65d2e585
Isa-cat <im.marquardts@gmail.com>
2025-06-30 13:23:39 +0200

    Update sequence_kmer_composition.py
    Add generation of adjacency list of patterns

--- sequence_kmer_composition.py
+++ sequence_kmer_composition.py
@@ -28,3 +28,36 @@ def kmers_to_string(kmers):
 print(kmers_to_string(f[0].split(' '))) 
 #print(f[0].split(' '))
 f = open("dataset_30182_3.txt", 'r').read().strip().splitlines('\n ')
+
+
+"""
+Overlap Graph: string adjacency problem
+input: collection of patterns
+output: adjacency list as a txt file
+"""
+from collections import defaultdict
+def adjacency_list(patterns):
+    adjacency_dict=defaultdict(list)
+    k=len(patterns[0])
+    for kmer in patterns:
+        for kmer2 in patterns:
+            if kmer!=kmer2:
+                if kmer[1:] == kmer2[0:k-1]:
+                    adjacency_dict[kmer].append(kmer2)
+            else:
+                pass
+    return adjacency_dict
+
+#print(adjacency_list(['ATGCG', 'GCATG', 'CATGC', 'AGGCA', 'GGCAT', 'GGCAC']))
+def write_to_doc(patterns):
+    edge_dict=adjacency_list(patterns)
+    with open("output.txt", "w") as file:
+        for key, value in edge_dict.items():
+            file.write(f"{key}: {' '.join(value)}\n")
+
+
+
+#write_to_doc(['ATGCG', 'GCATG', 'CATGC', 'AGGCA', 'GGCAT', 'GGCAC'])
+write_to_doc(f[0].split(' '))
+#print(f[0].split(' '))
+f = open("dataset_30182_10 (1).txt", 'r').read().strip().splitlines('\n ')
