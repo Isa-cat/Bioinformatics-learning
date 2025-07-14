3a3ccd1f0525018724e05b96b648d845ebdb8c5c
Isa-cat <im.marquardts@gmail.com>
2025-07-14 16:02:02 +0200

    Update eulerian_cycle.py
    Still inefficient but seems to work correctly

--- eulerian_cycle.py
+++ eulerian_cycle.py
@@ -33,7 +33,7 @@ def eulerian_cycle(adj_list):
     #graph_dict=defaultdict(list)
     #for i in range(len(nodes)):
        # graph_dict[nodes[i]]=edges[i]
-    print(graph_dict)
+    #print(graph_dict)
     nodes=list(graph_dict.keys())
     start_node=nodes[0]
     current_node=start_node
@@ -41,58 +41,53 @@ def eulerian_cycle(adj_list):
     #paths=[]
     cycle_path=deque(current_node)
     
-    while bool(graph_dict) == True:
+    while bool(graph_dict)== True:
         #cycle_dict=graph_dict
         #print(graph_dict.values())
         #if last_node in graph_dict.keys():
-        if (last_node in graph_dict.keys()) and (len(graph_dict[last_node])>1): 
+        if (last_node in graph_dict) and (len(graph_dict[last_node])>1): 
             current_node=str(graph_dict[last_node][random.randint(0,len(graph_dict[last_node])-1)]).strip("'[]'")
             graph_dict[last_node].remove(current_node)
-        elif (last_node in graph_dict.keys()) and (len(graph_dict[last_node])==1):
+        elif (last_node in graph_dict) and (len(graph_dict[last_node])==1):
             current_node=str(graph_dict[last_node]).strip("'['']'")
-            graph_dict.pop(last_node)
-        elif (last_node in graph_dict.keys()) and (graph_dict[last_node] is None):
-            graph_dict.pop(last_node)
-            #assign new curren_node to a value shown in the edge list of the dictionary
-        #else:
+            del(graph_dict[last_node])
+        elif (last_node in graph_dict) and (graph_dict[last_node] is None):
+            del(graph_dict[last_node])
             
-            
-        print(current_node)
-        cycle_path.append(current_node)
-        last_node=current_node
+        #print("items in dict :",len(graph_dict.keys()))      
+        last_node=current_node    
+        #print(current_node)
+        print(cycle_path)
+        #print(graph_dict)
+        if current_node in graph_dict:
+            cycle_path.append(current_node)
+        if (current_node not in graph_dict) and (len(graph_dict.keys()) == 0):
+            cycle_path.append(current_node)
+            break
         #print(cycle_path)
         #print(graph_dict)    
-                
-        if current_node not in graph_dict.keys():
+                 
+        elif (current_node not in graph_dict) and (len(graph_dict.keys()) > 0):
             #if the graph dict has had current_node removed (-> no way to go further)
-            #decide new starting point (index)
-            #paths.append(cycle_path)
-            new_index=random.randint(0,len(cycle_path)-1)
-            if start_node in graph_dict.keys():
-                graph_dict[start_node].append(cycle_path[cycle_path.index(start_node)+1])
+            #decide new starting point (new_index)
+            new_index=random.randint(1,len(cycle_path)-1)
+            #if start_node in graph_dict:
+                #graph_dict[start_node].append(cycle_path[cycle_path.index(start_node)+1])
                 #if the start node is still available as transit point: add the edge to second node again
                 #so that cycle when start point is changed can go that edge again
-            elif start_node not in graph_dict.keys():
-                graph_dict[start_node]=list(cycle_path[1])
-            current_node= int(cycle_path[new_index])
-            cycle_path.rotate(-current_node)
-            start_node=cycle_path[0]
-            current_node=cycle_path[-1]
-            last_node=cycle_path[-2]
-            print(cycle_path)
-            
-        #cycle_path.append(current_node)
+            #elif (start_node not in graph_dict) and (len(graph_dict.keys()) >0):
+                #graph_dict[start_node]=list(cycle_path[1])
+            cycle_path.rotate(-new_index-1)
+            last_node=cycle_path[-1]
+            print("New_index: ", new_index)
         
     return cycle_path
             
         
     
 def read_adj_list(adj_list):
-    #nodes=[]
-    #edges=[]
     nodes=[item.split(':')[0] for item in adj_list]
     #print(nodes)
-    #nodes, edgelist=[node, edges for node, edges in adj_list.split(':')]
     
     edges=map(str.split,[item.split(':')[1] for item in adj_list])
     graph_dict=dict(zip(nodes,edges))
